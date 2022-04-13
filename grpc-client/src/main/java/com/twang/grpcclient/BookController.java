b469ede3a7efb233f285b84dcf9422e5c9de4a4f
Wang, Tao <twang@telenav.cn>
2022-04-13 16:06:18 +0800

    add stream model

--- grpc-client/src/main/java/com/twang/grpcclient/BookController.java
+++ grpc-client/src/main/java/com/twang/grpcclient/BookController.java
@@ -6,12 +6,15 @@ import com.twang.proto.BookServiceGrpc;
 import com.twang.proto.HelloWorldServiceGrpc;
 import com.twang.proto.SearchRequest;
 import com.twang.proto.SearchResponse;
+import com.twang.proto.StreamResponse;
+import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.client.inject.GrpcClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.charset.Charset;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -22,6 +25,9 @@ public class BookController {
 
     @GrpcClient("gRPC server name")
     private BookServiceGrpc.BookServiceFutureStub bookServiceFutureStub;
+
+    @GrpcClient("gRPC server name")
+    private BookServiceGrpc.BookServiceStub bookServiceStub;
 
     @GetMapping("/list")
     public String list() throws ExecutionException, InterruptedException, TimeoutException {
@@ -37,5 +43,31 @@ public class BookController {
             System.out.println("bookName = " + bookName);
         }
         return response.toString();
+    }
+
+
+    @GetMapping("/stream")
+    public String stream() throws ExecutionException, InterruptedException, TimeoutException {
+        SearchRequest request = SearchRequest.newBuilder()
+                .setBookName("java")
+                .build();
+        bookServiceStub.queryBookStream(request, new StreamObserver<StreamResponse>() {
+            @Override
+            public void onNext(StreamResponse streamResponse) {
+                System.out.println("streamResponse = " + streamResponse.toString());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted =>> " + new Date());
+            }
+        });
+
+        return "success";
     }
 }
--- grpc-server/src/main/java/com/twang/grpcserver/BookService.java
+++ grpc-server/src/main/java/com/twang/grpcserver/BookService.java
@@ -4,8 +4,11 @@ import com.twang.proto.Book;
 import com.twang.proto.BookServiceGrpc;
 import com.twang.proto.SearchRequest;
 import com.twang.proto.SearchResponse;
+import com.twang.proto.StreamResponse;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+
+import java.util.concurrent.TimeUnit;
 
 @GrpcService
 public class BookService extends BookServiceGrpc.BookServiceImplBase {
@@ -25,6 +28,41 @@ public class BookService extends BookServiceGrpc.BookServiceImplBase {
                         .build())
                 .build();
         responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void queryBookStream(SearchRequest request, StreamObserver<StreamResponse> responseObserver) {
+        StreamResponse book1 = StreamResponse.newBuilder()
+                .setBook(Book.newBuilder()
+                        .setBookName("Java入门")
+                        .setBookType(Book.BookType.JAVA)
+                        .setAuthor("Gosling")
+                        .setPrice(99.99f)
+                        .setRemark("java是最好的语言")
+                        .setSaleStatus(true)
+                        .build())
+                .build();
+        responseObserver.onNext(book1);
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        StreamResponse book2 = StreamResponse.newBuilder()
+                .setBook(Book.newBuilder()
+                        .setBookName("CPP第五版")
+                        .setBookType(Book.BookType.CPP)
+                        .setAuthor("cpp")
+                        .setPrice(199.99f)
+                        .setRemark("cpp有点难")
+                        .setSaleStatus(true)
+                        .build())
+                .build();
+        responseObserver.onNext(book2);
+
         responseObserver.onCompleted();
     }
 }
